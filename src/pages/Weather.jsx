fe474874cfb92b9da61a7399fabd5fa9dcf7700c
Kathirvel Paramasivan <kathirvelparamasivan23@gmail.com>
2025-11-04 19:22:00 +0530

    Update Weather.jsx

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import Searchbtn from "../components/searchbtn";
+import Searchbtn from "../components/Searchbtn";
 import {
   WiDaySunny,
   WiNightClear,
